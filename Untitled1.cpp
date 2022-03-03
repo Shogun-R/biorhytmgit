91cd26fc6ff028084503d6f6f59d491abe6be97d
Shogun-R <58804006+Shogun-R@users.noreply.github.com>
2022-03-03 15:31:55 +0100

    Additional cals and stuff

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -7,15 +7,16 @@ float DateCalc(int currentYear, int yearOfBirth);
 
 struct Date
 {
-	string day;
-	string month;
-	string year;
+	int day;
+	int month;
+	int year;
 }date_of_birth;
 
 int main()
 {
 	const int YEAR_CONST = 1900;
 	const int MONTH_CONST = 1;
+	float biorhytm = 23.0f, biorhytmHalf = 11.5f;
 	time_t t = time(0);
 	tm* now = localtime(&t);
 	int currentYear = YEAR_CONST + now->tm_year;
@@ -36,13 +37,26 @@ int main()
    cout << now->tm_min << ":";
    cout << now->tm_sec << endl;
    
-   
+   cout<<"Full days in your birth date to current date difference: "<<DateCalc(currentYear, date_of_birth.year);
    
    
    return 0;
 }
 
-/*float DateCalc(int currentYear, int yearOfbirth)
+float DateCalc(int currentYear, int yearOfBirth)
 {
+	int fullDays, addedDays; //fullDays is a variable for amount of days pass from birth to current time | addedDays is for long years
+	int yearDifference, completeDays;
 	
-}*/
+	yearDifference = currentYear - yearOfBirth;
+	fullDays = yearDifference * 365;
+	addedDays = yearDifference / 4;
+	completeDays = fullDays + addedDays;
+	
+	return completeDays;
+}
+
+
+/*
+	Algorhytm for farther calcs year difference between year of birth and current year - full cycle of biorhytm to determine the current cycle day
+*/
